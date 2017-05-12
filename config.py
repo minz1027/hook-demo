e4a6ce8e753dc5fa7746eeabead132de7d43daa3
Min Zhang <minzhangcmu@gmail.com>
2017-05-12 10:45:02 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
- import os
+#/usr/bin/env python
+import os
  
- def main()
- PREFIX = '' if os.environ.get('PREFIX') is None else os.environ.get('PREFIX')
- print(PREFIX)
- 
- if __name__ == '__main__':
-    main()
+def main()
+PREFIX = '' if os.environ.get('PREFIX') is None else os.environ.get('PREFIX')
+print(PREFIX)
+
+if __name__ == '__main__':
+   main()
